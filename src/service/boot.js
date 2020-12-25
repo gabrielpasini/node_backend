872af6544d21a1ec5f8da6a2311037491697cb6d
gabrielpasini <gabrielpasini@outlook.com.br>
2020-12-24 21:49:11 -0300

    :memo: cors methods

--- src/service/boot.js
+++ src/service/boot.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
