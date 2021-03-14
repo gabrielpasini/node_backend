b98e76932119e812dfad411bfadd2e91180f7940
gabrielpasini <gabrielpasini@outlook.com.br>
2021-03-13 23:00:02 -0300

    :memo: secure true in email

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -5,7 +5,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: false,
+  secure: true,
   auth: {
     type: "login",
     user: process.env.EMAIL_USER,
