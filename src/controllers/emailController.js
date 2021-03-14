2e5b8004c6974405fa79eefd2d4b1cac1f06b61e
gabrielpasini <gabrielpasini@outlook.com.br>
2021-03-13 22:15:22 -0300

    :sparkles: uti_aux email endpoint

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -38,4 +38,27 @@ router.post("/gabriel", async (req, res) => {
   }
 });
 
+router.post("/uti-aux", async (req, res) => {
+  try {
+    const payload = req.body;
+    console.log(payload);
+    const email = {
+      from: `${payload.nome} <${process.env.EMAIL_USER}>`,
+      subject: `CONTATO VIA UTI_AUX - ${payload.assunto}`,
+      to: [payload.email, process.env.EMAIL_USER],
+      text: payload.mensagem,
+    };
+    await transporter.sendMail(email);
+    return res.status(200).send({
+      email,
+      status: "success",
+      message: "E-mail enviado com sucesso!",
+    });
+  } catch (err) {
+    return res
+      .status(400)
+      .send({ error: err, status: "error", message: "E-mail error!" });
+  }
+});
+
 module.exports = (app) => app.use("/email", router);
